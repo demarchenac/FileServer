fe2a077672bb7f0786bd83d1bdaeb05c687d8008
CDDemar <demarchenac@uninorte.edu.co>
2019-09-17 17:05:40 -0500

    Joins and leaves server pool on demand

--- src/main/java/com/mycompany/listeners/ServletContextManager.java
+++ src/main/java/com/mycompany/listeners/ServletContextManager.java
@@ -5,21 +5,28 @@
  */
 package com.mycompany.listeners;
 
-import com.google.gson.Gson;
-import com.mycompany.models.SinglePropQuery;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.net.URL;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPut;
+
+import com.google.gson.Gson;
+
+import com.mycompany.models.SinglePropQuery;
+import com.mycompany.utils.Constants;
 
 /**
  *
@@ -27,60 +34,110 @@ import javax.servlet.ServletContextListener;
  */
 public class ServletContextManager implements ServletContextListener{
 
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
+    private final Gson gson;
+    private final Properties props;
+    
+    public ServletContextManager() {
+        System.out.println("Started File Server -> [FS]");
+        System.out.println("[FS] Listener created!");
+        this.gson = new Gson();
+        
+        props = new Properties();
+            
         try {
-            System.out.println("--- Loading props ---");
-            Properties prop = new Properties();
+            System.out.println("[FS] Loading props");
             InputStream is = ServletContextManager.class.getClassLoader().getResourceAsStream("config.properties");
-            prop.load(is);
-            
-            System.out.println("--- Attempting to connect to server pool ---");
-            String uri = "http://" +prop.getProperty("WebPool") +":8080/WebPool/api/utils";
-            
-            System.out.println("Uri: " +uri);
-            
-            URL url = new URL(uri);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json; utf-8");
-            con.setRequestProperty("Accept", "application/json");
-            con.setDoOutput(true);
-            
-            Gson gson = new Gson();
-            String body = gson.toJson(new SinglePropQuery(InetAddress.getLocalHost().getHostAddress()));
-            
-            System.out.println("Body: " +body);
-            
-            try(OutputStream os = con.getOutputStream()) {
-                byte[] input = body.getBytes("utf-8");
-                os.write(input, 0, input.length);   
-                
-                try(
-                    BufferedReader br 
-                        = new BufferedReader(
-                            new InputStreamReader(con.getInputStream(), "utf-8")
-                        )
-                    ){
-                      StringBuffer sb = new StringBuffer();
-                      String line = null;
-                      while ((line = br.readLine()) != null) {
-                          sb.append(line.trim());
-                      }
-                      System.out.println(sb.toString());
-                }
-            }catch(IOException ioex){
-                ioex.printStackTrace();
-            }
+            props.load(is);
         } catch (IOException ex) {
             Logger.getLogger(ServletContextManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {    
+        System.out.println("[FS] Attempting to connect to server pool");
+
+        try {
+            registerFileServer(props.getProperty("WebPool"));
+        } catch (Exception ex) {
+            System.out.println("[FS] Error with the request logic.");
         }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        System.out.println("Bye Bye!");
+        System.out.println("[FS] Web-app suspending started! ");
+        try {
+            unregisterFileServer(props.getProperty("WebPool"));
+        } catch (Exception exception) {
+            System.out.println("[FS] Error with the request logic.");
+        }
+        System.out.println("[FS] Web-App stopped!");
     }
     
+    private void unregisterFileServer(String prop) throws Exception {
+
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String url = "http://" +prop +":8080/WebPool/api/utils" +"/" +ip;
+
+        HttpClient client = HttpClients.createDefault();
+        HttpDelete request = new HttpDelete(url);
+
+        // add request header
+        request.addHeader("User-Agent", Constants.USER_AGENT);
+
+        HttpResponse response = client.execute(request);
+
+        System.out.println("[FS] Unregistering server from load balancer service!");
+        System.out.println("Response Code : " + 
+               response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+               new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+                result.append(line);
+        }
+
+        System.out.println("[FS] " +result.toString());
+    }
+    
+    private void registerFileServer(String prop) throws Exception {
+
+        String url = "http://" +prop +":8080/WebPool/api/utils";
+
+        HttpClient client = HttpClients.createDefault();
+        HttpPut request = new HttpPut(url);
+
+        // add header
+        request.setHeader("User-Agent", Constants.USER_AGENT);
+
+        StringEntity entity 
+            = new StringEntity(
+                gson.toJson(
+                    new SinglePropQuery(
+                        InetAddress.getLocalHost().getHostAddress())));
+        
+        request.setEntity(entity);
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+
+        System.out.println("[FS] Sending register request");
+        HttpResponse response = client.execute(request);
+        System.out.println("[FS] Response Code : " + 
+                            response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+                result.append(line);
+        }
+
+        System.out.println("[FS] "+result.toString());
+    }
 }
--- src/main/java/com/mycompany/services/FileController.java
+++ src/main/java/com/mycompany/services/FileController.java
@@ -7,13 +7,22 @@ package com.mycompany.services;
 
 import com.google.gson.Gson;
 import com.mycompany.models.Response;
+import com.mycompany.models.SinglePropQuery;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
 
 /**
  *
