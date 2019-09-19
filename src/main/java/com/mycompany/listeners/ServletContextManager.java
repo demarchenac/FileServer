754bf6ad95109481c43631ceef3990b2335f72e2
Cristhyan De Marchena <demarchenac@uninorte.edu>
2019-09-18 22:56:01 -0500

    Added files folder existance verification!

--- src/main/java/com/mycompany/listeners/ServletContextManager.java
+++ src/main/java/com/mycompany/listeners/ServletContextManager.java
@@ -15,6 +15,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.File;
+import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -24,12 +26,9 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPut;
 
 import com.google.gson.Gson;
-import com.mycompany.models.ServerRegistrationQuery;
 
-import com.mycompany.models.SinglePropQuery;
+import com.mycompany.models.ServerRegistrationQuery;
 import com.mycompany.utils.Constants;
-import java.io.File;
-import java.util.ArrayList;
 
 /**
  *
@@ -37,8 +36,6 @@ import java.util.ArrayList;
  */
 public class ServletContextManager implements ServletContextListener{
 
-    
-    private final String FILE_FOLDER = "D://DISTRIBUIDA/P1/FILES";
     private final Properties props;
     private final Gson gson;
     
@@ -95,15 +92,15 @@ public class ServletContextManager implements ServletContextListener{
 
         System.out.println("[FS] Unregistering server from load balancer service!");
         System.out.println("Response Code : " + 
-               response.getStatusLine().getStatusCode());
+            response.getStatusLine().getStatusCode());
 
         BufferedReader rd = new BufferedReader(
-               new InputStreamReader(response.getEntity().getContent()));
+            new InputStreamReader(response.getEntity().getContent()));
 
         StringBuffer result = new StringBuffer();
         String line = "";
         while ((line = rd.readLine()) != null) {
-                result.append(line);
+            result.append(line);
         }
 
         System.out.println("[FS] " +result.toString());
@@ -119,18 +116,26 @@ public class ServletContextManager implements ServletContextListener{
         // add header
         request.setHeader("User-Agent", Constants.USER_AGENT);
 
-        ArrayList<String> fileList = new ArrayList<String>();
-        File folder = new File(FILE_FOLDER);
+        ArrayList<String> fileList = new ArrayList<>();
+        
+        File folder = new File(props.getProperty("ROOT_FOLDER"));
+        
+        if(! folder.exists()){
+            folder.mkdirs();
+        }
+        
         File[] listOfFiles = folder.listFiles();
 
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-              fileList.add(listOfFiles[i].getName());
+        for (File listOfFile : listOfFiles) {
+            if (listOfFile.isFile()) {
+                fileList.add(listOfFile.getName());
             }
         }
         
         ServerRegistrationQuery srq 
-                = new ServerRegistrationQuery(InetAddress.getLocalHost().getHostAddress(), fileList);
+            = new ServerRegistrationQuery(
+                InetAddress.getLocalHost().getHostAddress(), 
+                fileList);
         
         StringEntity entity = new StringEntity(gson.toJson(srq));
         
@@ -140,16 +145,17 @@ public class ServletContextManager implements ServletContextListener{
 
         System.out.println("[FS] Sending register request");
         HttpResponse response = client.execute(request);
-        System.out.println("[FS] Response Code : " + 
-                            response.getStatusLine().getStatusCode());
+        System.out.println(
+            "[FS] Response Code : " + 
+            response.getStatusLine().getStatusCode());
 
         BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
+            new InputStreamReader(response.getEntity().getContent()));
 
         StringBuffer result = new StringBuffer();
         String line = "";
         while ((line = rd.readLine()) != null) {
-                result.append(line);
+            result.append(line);
         }
 
         System.out.println("[FS] "+result.toString());
