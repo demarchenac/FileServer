03a30777271f00b9b485b5fe22535228e5bdefa8
demarchenac <demarchenac@SU423-109862-L.CampusUninorte.local>
2019-09-26 19:23:35 -0500

    FileServer downloads missing files on startup!

--- src/main/java/com/mycompany/listeners/ServletContextManager.java
+++ src/main/java/com/mycompany/listeners/ServletContextManager.java
@@ -26,9 +26,12 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPut;
 
 import com.google.gson.Gson;
+import com.mycompany.models.Response;
 
 import com.mycompany.models.ServerRegistrationQuery;
 import com.mycompany.utils.Constants;
+import java.io.FileOutputStream;
+import org.apache.http.client.methods.HttpGet;
 
 /**
  *
@@ -61,6 +64,23 @@ public class ServletContextManager implements ServletContextListener{
 
         try {
             registerFileServer(props.getProperty("WebPool"));
+            
+            System.out.println("[FS] Checking files that could be missing...");
+            
+            ArrayList<String> missingFiles 
+                    = obtainFileList(props.getProperty("WebPool"));
+            
+            System.out.println("[FS] Files missing: ");
+            System.out.println(gson.toJson(missingFiles));
+            
+            String response = "";
+            for(String filename : missingFiles){
+                System.out.println("[FS] Attempting to download File: " +filename);
+                do{
+                    response = donwloadFile(props.getProperty("WebPool"), filename);
+                }while(response.equals("retry"));
+            }
+            
         } catch (Exception ex) {
             System.out.println("[FS] Error with the request logic.");
         }
@@ -159,5 +179,111 @@ public class ServletContextManager implements ServletContextListener{
         }
 
         System.out.println("[FS] "+result.toString());
+    }
+
+    private ArrayList<String> obtainFileList(String serverPoolIp) throws Exception{
+        ArrayList<String> filesMissing = new ArrayList<>();
+        String url = "http://" +serverPoolIp +":8080/WebPool/api/files";
+        
+        HttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(url);
+
+        // add request header
+        request.addHeader("User-Agent", Constants.USER_AGENT);
+
+        HttpResponse response = client.execute(request);
+
+        System.out.println("[FS] Requesting file list from server pool!");
+        System.out.println("Response Code : " + 
+            response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+            new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        System.out.println("[FS] " +result.toString());
+        
+        Response noJson = gson.fromJson(result.toString(), Response.class);
+        
+        ArrayList<String> filesFromServer 
+            = gson.fromJson(noJson.getData(), ArrayList.class);
+        
+        ArrayList<String> fileList = new ArrayList<>();
+        
+        File folder = new File(props.getProperty("ROOT_FOLDER"));
+        
+        if(! folder.exists()){
+            folder.mkdirs();
+        }
+        
+        File[] listOfFiles = folder.listFiles();
+
+        for (File listOfFile : listOfFiles) {
+            if (listOfFile.isFile()) {
+                fileList.add(listOfFile.getName());
+            }
+        }
+        
+        if(filesFromServer.size() > 0){
+            for(String file : filesFromServer){
+                if(!fileList.contains(file)){
+                    filesMissing.add(file);
+                }
+            }
+        }
+        
+        return filesMissing;
+    }
+
+    private String donwloadFile(String serverPoolIp, String filename){
+        String message = "retry";
+        int inByte;
+        try {
+            InputStream is = downloadFileFromServer(serverPoolIp, filename);
+            if(is == null){
+                message = "404";
+            }else{
+                FileOutputStream fos 
+                    = new FileOutputStream(
+                        new File(props.getProperty("ROOT_FOLDER") + "/" + filename));
+                 while((inByte = is.read()) != -1){
+                    fos.write(inByte);
+                }
+                is.close();
+                fos.close();
+                message = "success!";
+            }
+        } catch (Exception ex) {
+            message = "retry";
+        }
+        return message;
+    }
+
+    public InputStream downloadFileFromServer(String server, String filename) throws Exception {
+        HttpResponse response = null;
+        try {
+            filename.replaceAll(" ", "%20");
+            String url = "http://" + server + ":8080"  + "/WebPool/api/files" + "/" + filename;
+
+            HttpClient client = HttpClients.createDefault();
+            HttpGet request = new HttpGet(url);
+
+            // add request header
+            request.addHeader("User-Agent", Constants.USER_AGENT);
+
+            System.out.println("[FS] Requesting " + filename + " to server-pool@" + server);
+
+            response = client.execute(request);
+
+            return response.getEntity().getContent();
+        } catch (Exception e) {
+            return null;
+        }
+        
     }
 }
